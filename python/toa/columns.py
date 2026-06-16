77b1ef1a6a67d16aec17a0b6bc14085655aaa336
tor-gu <tor.gunston@protonmail.com>
2026-06-15 21:28:30 -0400

    Add constants for column names

---
+++ python/toa/columns.py
@@ -0,0 +1,17 @@
+class NamesCol:
+    ID = "id"
+    ARTIST = "artist"
+    ALBUM = "album"
+
+
+class ResultsCol:
+    MATCH_ID = "match_id"
+    DATE = "date"
+    ORDER = "order"
+
+
+class ScoresCol:
+    ID = "id"
+    SCORE = "score"
+    ROBUSTNESS = "robustness"
+    DATE = "date"
