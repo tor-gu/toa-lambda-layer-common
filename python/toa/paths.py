bffc132b507c61720378776ff72f66c30ee1ad97
tor-gu <tor.gunston@protonmail.com>
2026-06-20 08:38:37 -0400

    Add common paths

---
+++ python/toa/paths.py
@@ -0,0 +1,8 @@
+NAMES_UNPROCESSED_PREFIX = "names/unprocessed/"
+NAMES_CONSOLIDATED_KEY = "names/consolidated/names.parquet"
+
+RESULTS_UNPROCESSED_PREFIX = "results/unprocessed/"
+RESULTS_PROCESSED_PREFIX = "results/processed/"
+RESULTS_CONSOLIDATED_KEY = "results/consolidated/results.parquet"
+
+SCORES_KEY = "scores/scores.parquet"
